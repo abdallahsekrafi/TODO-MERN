8f8aa6d384d9197e148168369bd63b4b894169f9
Sekrafi Abdallah <abdallah.sek@gmail.com>
2025-06-30 15:32:08 +0100

    Update todoService.js

--- client/src/services/todoService.js
+++ client/src/services/todoService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = process.env.REACT_APP_API_URL;
 export default {
   getTodos: () => axios.get(`${BASE_URL}/`),
   // getTodos: async () => {
